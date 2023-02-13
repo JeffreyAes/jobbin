bf416993ad63f0a1ef511d077b1167daf0be9dc7
Jeffrey <aeschlimanjeffrey@gmail.com>
2023-02-12 21:36:41 -0800

    some styling fixes

--- client/src/components/BoardTable.jsx
+++ client/src/components/BoardTable.jsx
@@ -10,25 +10,35 @@ const BoardTable = (props) => {
     return (
         <div className="">
 
-        {loaded && user.board.map((board, i) => 
-            <div key={i} className="d-flex justify-content-center">
-                {board.table.list.map((list, i) =>
-                <table className='table table-striped'>
-                    <thead>
-                        <tr>
-                            <th key={i}>{list.name}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td key={i}>{list.value.length > 0? list.value : "‎  " }</td>
-                        </tr>
-                    </tbody>
-                </table>
-                )}
-            </div>
+            {loaded && user.board.map((board, i) =>
+                <div key={i} className="d-flex justify-content-center">
+                    <table className='table table-striped'>
+                        <thead>
+                            <tr>
+                                {board.table.list.map((list, i) =>
+                                    <th key={i} >{list.name} <button>delete</button></th>
+                                )}
+                                <td key={i}><button className='btn btn-info text-white'>Add List</button></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                {board.table.list.map((list, i) =>
+                                    <td key={i}><button className='btn btn-info text-white'>+</button></td>
+                                )}
+                                <td></td>
+                            </tr>
+                            <tr>
+                                {board.table.list.map((list, i) =>
+                                    <td key={i}>{list.value.length > 0 ? list.value  : "‎  "  } </td>
+                                )}
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             )}
-            </div>
+        </div>
     )
 }
 
