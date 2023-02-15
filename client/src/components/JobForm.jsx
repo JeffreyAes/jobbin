049887ede31de3d927e61ddb90370fbc161d7f5f
Jeffrey <aeschlimanjeffrey@gmail.com>
2023-02-15 11:38:10 -0800

    job selection works now

--- client/src/components/JobForm.jsx
+++ client/src/components/JobForm.jsx
@@ -4,9 +4,10 @@ import { useParams } from 'react-router-dom'
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-// import InputLabel from '@mui/material/InputLabel';
-// import MenuItem from '@mui/material/MenuItem';
-// import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import { InputLabel } from '@mui/material';
 
 
 const JobForm = (props) => {
@@ -16,13 +17,13 @@ const JobForm = (props) => {
     const tableIndex = props.tableIndex
     const [jobTitle, setJobTitle] = useState("")
     const [company, setCompany] = useState("")
-    // const [newBoardIndex, setNewBoardIndex] = useState(boardIndex)
-    // const [newTableIndex, setNewTableIndex] = useState(tableIndex)
+    const [newBoardIndex, setNewBoardIndex] = useState(boardIndex)
+    const [newTableIndex, setNewTableIndex] = useState(tableIndex)
 
     const handleSubmit = e => {
         e.preventDefault();
         let arr = user.board
-        let newArr = arr[boardIndex].table.list[tableIndex].value
+        let newArr = arr[newBoardIndex].table.list[newTableIndex].value
         newArr.push([jobTitle, company])
         axios.put('http://localhost:8000/api/users/' + id, {
             board: arr
@@ -31,7 +32,7 @@ const JobForm = (props) => {
                 props.setRerender(true)
                 props.setShowJobForm(false)
                 console.log(res)
-                console.log(boardIndex)
+                console.log(tableIndex)
             })
             .catch(err => {
                 console.log(err)
@@ -40,70 +41,82 @@ const JobForm = (props) => {
 
     const returnToDashboard = () => {
         props.setShowJobForm(false)
-        console.log(boardIndex)
+        console.log(tableIndex)
     }
 
     return (
-        <div>
-            <Button type='button' variant="contained" color="secondary"onClick={() => {
+        <div className='container'>
+            <Button type='button' variant="contained" color="secondary" onClick={() => {
                 returnToDashboard()
             }}>Back</Button>
             <Box
                 component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
+                sx={{ minWidth: 120 }}
                 noValidate
                 autoComplete="off"
                 onSubmit={handleSubmit}
             >
 
-                <TextField
-                    required
-                    id="jobTitle"
-                    label="Job Title"
-                    type="text"
-                    variant="standard"
-                    onChange={(e) => setJobTitle(e.target.value)} value={jobTitle}
-                />
-                <TextField
-                    required
-                    id="company"
-                    label="Company"
-                    type="text"
-                    variant="standard"
-                    onChange={(e) => setCompany(e.target.value)} value={company}
-                />
 
-                {/* <div className='d-flex me-3 gap-3 justify-content-center'>
-                    <Select
-                        id="board"
-                        defaultValue={boardIndex}
-                        label="Board"
-                        onChange={(e) => setNewBoardIndex(e.target.value)}
-                    >
-                        {user.board.map((board, i) => {
+                <FormControl sx={{ m: 1, minWidth: 120 }}>
 
-                            <MenuItem key={i} value={i}>{board.boardName}</MenuItem>
+                    <TextField
+                        required
+                        id="jobTitle"
+                        label="Job Title"
+                        type="text"
+                        variant="standard"
+                        onChange={(e) => setJobTitle(e.target.value)} value={jobTitle}
+                    />
+                </FormControl>
+                <FormControl sx={{ m: 1, minWidth: 120 }}>
 
-                        })
-                        }
-                    </Select>
+                    <TextField
+                        required
+                        id="company"
+                        label="Company"
+                        type="text"
+                        variant="standard"
+                        onChange={(e) => setCompany(e.target.value)} value={company}
+                    />
+                </FormControl>
+                <div >
 
-                    <Select
-                        id="table"
-                        defaultValue={tableIndex}
-                        label="Board"
-                        onChange={(e) => setNewTableIndex(e.target.value)} value={newTableIndex}
-                    >
-                        {user.board[newBoardIndex].table.list.map((table, i) => {
+                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                        <InputLabel id="demo-select-small">Board</InputLabel>
 
-                            <MenuItem key={i} value={i}>{table.name}</MenuItem>
+                        <Select
+                            id="board"
+                            defaultValue={boardIndex}
+                            label="Board"
+                            onChange={(e) => setNewBoardIndex(e.target.value)}
+                        >
+                            {user.board.map((board, i) => {
 
-                        })
-                        }
-                    </Select>
-                </div> */}
+                                return <MenuItem key={i} value={i}>{board.boardName}</MenuItem>
+
+                            })
+                            }
+                        </Select>
+                    </FormControl>
+                    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                        <InputLabel id="">Table</InputLabel>
+
+                        <Select
+                            id="table"
+                            defaultValue={tableIndex}
+                            label="Board"
+                            onChange={(e) => setNewTableIndex(e.target.value)}
+                        >
+                            {user.board[newBoardIndex].table.list.map((table, i) => {
+
+                                return <MenuItem key={i} value={i}>{table.name}</MenuItem>
+
+                            })
+                            }
+                        </Select>
+                    </FormControl>
+                </div>
                 <Button type='submit' variant="contained" color="success">Save Job</Button>
             </Box>
         </div >
